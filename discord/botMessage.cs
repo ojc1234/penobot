1dd613702badedbf10a8b9f1f08f9685b93d680f
ojc1234 <54547113+ojc1234@users.noreply.github.com>
2023-01-30 00:36:36 +0900

    .쳤을때 행렬 매서드 구현 예제

--- discord/botMessage.cs
+++ discord/botMessage.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using penodiscordbot.matrix;
 namespace penodiscordbot.discord
 {
     internal class botMessage
@@ -31,6 +31,14 @@ namespace penodiscordbot.discord
                 {
                     await message.Channel.SendMessageAsync(embed: new discordEmbed()?.dictEmbed(diction)?.Build());
                 }
+            }
+            if (message.Content.FirstOrDefault().ToString() == ".")
+            {
+                string realMessage = message.Content.Remove(0,1);
+                List<int> inputs = realMessage.Split(' ').ToList().ConvertAll((i)=>int.Parse(i));
+                if (inputs.Count == 4) Console.WriteLine("List size ok");
+                var mat = new Matrix(new List<int> { inputs[0], inputs[1] }, new List<int> { inputs[2], inputs[3] });
+                await message.Channel.SendMessageAsync(embed: new discordEmbed()?.matrixEmbed(mat)?.Build());
             }
         }
     }
--- discord/discordEmbed.cs
+++ discord/discordEmbed.cs
@@ -1,6 +1,8 @@
 ﻿using Discord;
 using System.Collections.Generic;
+using penodiscordbot.matrix;
 namespace penodiscordbot.discord
+ 
 {
     internal class discordEmbed
     {
@@ -9,6 +11,13 @@ namespace penodiscordbot.discord
             var eb = new EmbedBuilder { Title = dict.englishWord, Description = dict.phenomenon?.discordString() };
             eb.AddField("뜻",dict.mean);
             eb.AddField("출처", request.ahaUrl(dict.englishWord));
+            return eb;
+        }
+        public EmbedBuilder matrixEmbed(Matrix matrix)
+        {
+            var eb = new EmbedBuilder { Title = "만들어진 행렬" };
+            eb.AddField("1", $"{matrix.Fildmatrix[0][0]} {matrix.Fildmatrix[0][1]}");
+            eb.AddField("2", $"{matrix.Fildmatrix[1][0]} {matrix.Fildmatrix[1][0]}");
             return eb;
         }
     }

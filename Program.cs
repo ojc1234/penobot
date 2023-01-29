1ca301513496daf6d7490a1111eb0ce9cf295f1b
ojc1234 <54547113+ojc1234@users.noreply.github.com>
2023-01-29 23:01:07 +0900

    오류났을때 작동멈추는거 수정 행렬연산 라이브러리 입력

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace penodiscordbot
 {
     class Program
@@ -9,16 +9,20 @@ namespace penodiscordbot
             int setting = 0;
             if (setting == 0)
             {
-                var disbot = new bot();
                 new bot().MainAsync().GetAwaiter().GetResult();
             }
             if (setting == 1)
             {
-             Console.WriteLine(request.Diction(request.ahaUrl("hello")).mean);
+                Console.WriteLine(request.Diction(request.ahaUrl("hello")).mean);
             }
             if (setting == 2)
             {
                 Console.WriteLine((new token()).discordtoken);
+            }
+            if (setting == 3)
+            {
+                var martix2d = new martix.martix(new List<int> { 1, 2 }, new List<int> { 3, 4 });
+                martix2d.Write();
             }
         }
     }
